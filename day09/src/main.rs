627870c77e81303948e2955af348443405f6464f
Markus Sons <markus.sons@gmail.com>
2024-12-11 15:43:46 +0100

    implemented day09

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,65 +1,101 @@
 use std::fs;
 
-fn parse_disk_map(disk_map: &str) -> Vec<char> {
-    let mut blocks = Vec::new();
-    for (i, c) in disk_map.chars().enumerate() {
-        let length = c.to_digit(10).unwrap();
-        if i % 2 == 0 {
-            // File blocks, use file IDs
-            blocks.extend(vec![(i / 2).to_string().chars().next().unwrap(); length as usize]);
-        } else {
-            // Free space, represented by '.'
-            blocks.extend(vec!['.'; length as usize]);
+const EMPTY: u64 = u64::MAX;
+
+fn main() {
+    let input = fs::read_to_string("input2.txt").expect("Unable to read file");
+    let mut disk = vec![];
+    let max_id = (input.len() - 1) as u64 / 2;
+
+    for (i, c) in input.chars().enumerate() {
+        let size = c.to_digit(10).unwrap();
+
+        for _ in 0..size {
+            if i % 2 == 0 {
+                disk.push((i as u64) / 2);
+            } else {
+                disk.push(EMPTY);
+            }
         }
     }
-    blocks
-}
 
-fn move_blocks(disk: &mut Vec<char>) {
+    let initial_disk = disk.clone();
+
+    // println!("{:?}", disk);
 
     loop {
-        // Find the rightmost file block
-        if let Some(file_index) = disk.iter().rposition(|&c| c != '.') {
-            // Find the leftmost free space
-            if let Some(free_index) = disk.iter().position(|&c| c == '.') {
-                if free_index < file_index {
-                    // Move the file block to the leftmost free space
-                    disk[free_index] = disk[file_index];
-                    disk[file_index] = '.';
-                    continue;
+        let last_file_pos: usize = disk.iter().rposition(|n| *n != EMPTY).unwrap();
+
+        let first_empty_pos = disk.iter().position(|n| *n == EMPTY).unwrap();
+
+        if first_empty_pos < last_file_pos {
+            disk[first_empty_pos] = disk[last_file_pos];
+            disk[last_file_pos] = EMPTY;
+        } else {
+            break;
+        }
+    }
+    // println!("{:?}", disk);
+
+    let sum: u64 = disk
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &c)| match c {
+            EMPTY => None,
+            _ => Some((i as u64) * c),
+        })
+        .sum();
+
+    println!("Part 1: {}", sum);
+
+    let mut disk = initial_disk.clone();
+
+    for id in (0..=max_id).rev() {
+        let file_pos: usize = disk.iter().position(|n| *n == id).unwrap();
+
+        let mut size = 1;
+
+        while disk.get(file_pos + size) == Some(&id) {
+            size += 1;
+        }
+
+        // find leftmost empty spot which is large enough
+        let mut empty_pos = disk.iter().position(|n| *n == EMPTY).unwrap();
+
+        loop {
+            let mut gap_size = 1;
+
+            while disk.get(empty_pos + gap_size) == Some(&EMPTY) {
+                gap_size += 1;
+            }
+
+            if empty_pos >= file_pos {
+                break;
+            }
+
+            if gap_size >= size {
+                for i in 0..size {
+                    disk[empty_pos + i] = id;
+                    disk[file_pos + i] = EMPTY;
+                }
+                break;
+            } else {
+                empty_pos += gap_size;
+                while disk.get(empty_pos) != Some(&EMPTY) {
+                    empty_pos += 1;
                 }
             }
         }
-        break; // Stop if no more moves can be made
     }
 
-}
-
-fn calculate_checksum(disk: &Vec<char>) -> u64 {
-    disk.iter()
+    let sum: u64 = disk
+        .iter()
         .enumerate()
-        .filter_map(|(pos, &block)| {
-            if block != '.' {
-                Some(pos as u64 * block.to_digit(10).unwrap() as u64)
-            } else {
-                None
-            }
+        .filter_map(|(i, &c)| match c {
+            EMPTY => None,
+            _ => Some((i as u64) * c),
         })
-        .sum()
-}
+        .sum();
 
-fn main() {
-    let disk_map = fs::read_to_string("input2.txt").expect("Unable to read file");
-
-    // Parse the input into the initial disk layout
-    let mut disk = parse_disk_map(&disk_map);
-
-    // Simulate the block movements
-    move_blocks(&mut disk);
-
-    // Calculate and print the checksum
-    let checksum = calculate_checksum(&disk);
-    println!("\nFinal Checksum: {}", checksum);
-    // println!("\n{}", disk.iter().collect::<String>());
-
+    println!("Part 2: {}", sum);
 }
