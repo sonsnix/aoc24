283c482d1d4894bcc2a6931547d43b2fdb3b5751
Markus Sons <markus.sons@gmail.com>
2024-12-26 10:45:28 +0100

    started day24

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,3 +1,25 @@
+use std::collections::{HashMap, HashSet};
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input.txt").unwrap();
+
+    let mut graph = HashMap::new();
+
+    for line in input.lines() {
+        let mut parts = line.split("-");
+        let computer_a = parts.next().unwrap();
+        let computer_b = parts.next().unwrap();
+
+        graph
+            .entry(computer_a)
+            .or_insert(HashSet::new())
+            .insert(computer_b);
+
+        graph
+            .entry(computer_b)
+            .or_insert(HashSet::new())
+            .insert(computer_a);
+    }
+
+    
 }
